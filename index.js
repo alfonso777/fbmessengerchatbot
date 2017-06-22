3a5b0378c611a01643c7fd0a7fd9686abc2d2b87
Alfonso Phocco Diaz <adiaz@itradeware.com>
2017-06-22 01:10:22 -0300

    chatbot set up

--- index.js
+++ index.js
@@ -6,30 +6,62 @@ const request = require('request')
 const app = express()
 
 const fb_page_token = process.env.FB_PAGE_ACCESS_TOKEN
+const verify_token = fb_page_token
 app.set('port', (process.env.PORT || 80))
 
 // Process application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({extended: false}))
 
-// Process application/json
+// To process application/json
 app.use(bodyParser.json())
 
-// Index route
+// root route
 app.get('/', function (req, res) {
 	res.send('Hello world, I am a chat bot')
 })
 
 //verification
 app.get('/webhook/', function (req, res) {
-	console.log('token: '+fb_page_token+', verifytoken:' + req.query['hub.verify_token']+', challenge: ' + req.query['hub.challenge'] +', equal?: ' + (req.query['hub.verify_token'] === fb_page_token));
-	if (req.query['hub.verify_token'] === fb_page_token) {
+	if (req.query['hub.verify_token'] === verify_token) {
 		res.send(req.query['hub.challenge'])
 	}else{
 		res.send('Error, wrong token')
 	}
 })
 
-//Spin up the server
+app.post('/webhook/', function (req, res) {
+    let messaging_events = req.body.entry[0].messaging
+    for (let i = 0; i < messaging_events.length; i++) {
+	    let event = req.body.entry[0].messaging[i]
+	    let sender = event.sender.id
+	    if (event.message && event.message.text) {
+		    let text = event.message.text
+		    sendTextMessage(sender, "Received message: " + text.substring(0, 200))
+	    }
+    }
+    res.sendStatus(200)
+})
+
+function sendTextMessage(sender, text) {
+    let message = { text:text }
+    request({
+	    url: 'https://graph.facebook.com/v2.6/me/messages',
+	    qs: {access_token: fb_page_token},
+	    method: 'POST',
+		json: {
+		    recipient: {id:sender},
+			message: message,
+		}
+	}, function(error, response, body) {
+		if (error) {
+		    console.log('Error sending messages: ', error)
+		} else if (response.body.error) {
+		    console.log('Error: ', response.body.error)
+	    }
+    })
+}
+
+//Running server
 app.listen(app.get('port'), function() {
 	console.log('running on port', app.get('port'))
 })
